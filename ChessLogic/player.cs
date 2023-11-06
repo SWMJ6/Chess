acb60d232b00b5665d492f07caf8475a5ff0905b
sulim <sulim@SULIMAN-PC>
2023-11-06 06:53:02 +0100

    Add project files.

---
+++ ChessLogic/player.cs
@@ -0,0 +1,22 @@
+﻿namespace ChessLogic
+{
+    public enum player
+    {
+        None,
+        White,
+        Black
+
+    }
+    public static class PlayerExtensions
+    {
+        public static player opponent(this player player)
+        {
+            return player switch
+            {
+                player.White => player.Black,
+                player.Black => player.White,
+                _ => player.None,
+            };
+        }
+    }
+}
